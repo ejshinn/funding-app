cea190c42cdb31bf9f3a2f7249a7b10acc4ad9a7
Ssong0515 <406605@naver.com>
2024-09-03 18:12:12 +0900

    spring 초기화

---
+++ spring/spring_fundingApp/src/main/java/bitc/fullstack405/spring_fundingapp/SpringFundingAppApplication.java
@@ -0,0 +1,13 @@
+package bitc.fullstack405.spring_fundingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFundingAppApplication.class, args);
+    }
+
+}
---
+++ spring/spring_fundingApp/src/test/java/bitc/fullstack405/spring_fundingapp/SpringFundingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package bitc.fullstack405.spring_fundingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFundingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
