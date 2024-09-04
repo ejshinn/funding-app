a926bf0be67e894683afd79c4fd53120f10e5eee
koy1208 <kkoy120806@gmail.com>
2024-09-04 16:44:58 +0900

    TODO 추가
    retrofit 사용 시 FunClient에 baseurl 값 수정 필요

--- android/app/src/main/java/com/example/myapplication/Retrofit/FunClient.kt
+++ android/app/src/main/java/com/example/myapplication/Retrofit/FunClient.kt
@@ -5,7 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object FunClient {
     val retrofit:FunInterface = Retrofit.Builder()
-        .baseUrl("")
+        //TODO: ip 주소 변경 필요
+        .baseUrl("http://ip주소:8877")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
         .create(FunInterface::class.java)
