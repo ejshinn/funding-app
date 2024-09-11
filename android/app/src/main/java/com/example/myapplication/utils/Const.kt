6d8ad3b671549714b9a8178c79758852192aa2be
Ssong0515 <406605@naver.com>
2024-09-11 11:03:23 +0900

    정상작동 확인

--- android/app/src/main/java/com/example/myapplication/utils/Const.kt
+++ android/app/src/main/java/com/example/myapplication/utils/Const.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.utils
 object Const {
     val SHARED_PREF_LOGIN_NAME = "loginCheck"
     val SHARED_PREF_LOGIN_KEY = "isLogin"
+    val SHARED_PREF_LOGIN_ID = ""
     val SHARED_PREF_LOGIN_TRUE = "true"
     val SHARED_PREF_LOGIN_FALSE = "false"
 
